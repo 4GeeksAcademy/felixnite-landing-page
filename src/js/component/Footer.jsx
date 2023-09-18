7a8cddf9c4e097eb4615a6b365bb9c13bc908170
Felixnite <141184414+Felixnite@users.noreply.github.com>
2023-09-18 06:09:53 +0000

    Builded the components

---
+++ src/js/component/Footer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Footer= () => {
+    <footer style="background-color: #333; color: #fff;">
+  <p>This is the footer text.</p>
+</footer>
+
+}
+
+export default Footer
